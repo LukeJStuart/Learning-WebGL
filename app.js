b74cda585cf096b7d0072081ffae37d941f62304
LukeJStuart <44208148+LukeJStuart@users.noreply.github.com>
2020-01-24 18:02:52 +0000

    Tried changing the colur of the second triangle.

--- app.js
+++ app.js
@@ -148,8 +148,8 @@ var InitDemo = function () {
         -0.5, -0.5,  0.7, 0.0, 1.0,
         0.5, -0.5,   0.1, 1.0, 0.6,
         1.0, 0.5,    0.5, 0.0, 0.5,
-        0.5, -0.5,   0.1, 1.0, 0.6,
-        0.0, 0.5,    1.0, 1.0, 0.0
+        0.5, -0.5,   1.0, 0.1, 0.6,
+        0.0, 0.5,    0.0, 1.0, 1.0
     ];
     // At this stage, triangleVertices is in the RAM, the graphics
     // card (so, also the vertex shader) has no knowledge of it yet
