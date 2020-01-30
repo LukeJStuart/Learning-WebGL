c603060d89bf9216013a8521b033d1e817cb88e2
LukeJStuart <44208148+LukeJStuart@users.noreply.github.com>
2020-01-30 14:26:23 +0000

    Started making changes to js code from tutorial 1 for this tuorial.

---
+++ Tutorial2/app.js
@@ -0,0 +1,156 @@
+var vertexShaderText =
+[
+'precision mediump float;',
+'',
+// As we are now working in 3D space, we make vertPosition
+// a vec3.
+'attribute vec3 vertPosition;',
+'attribute vec3 vertColour;',
+'varying vec3 fragColour;',
+// The matrices we will be using for transformations are the
+// same for every vertex, but are still inputs.
+// We will therfore use uniforms.
+// mat4 is a 4x4 matrix from glMatrix.
+'uniform mat4 mWorld;',
+'uniform mat4 mView;',
+'uniform mat4 mProj;',
+'',
+'void main()',
+'{',
+'   fragColour = vertColour;',
+// In matrix multiplication, operations are carried out in
+// order from right to left - so in this example, first
+// there is a position, then it is multiplied by the mWorld
+// matrix (rotating it in 3D space), then it is multiplied
+// by mView (where our camera is sitting), then it is
+// multiplied by mProj (to get us nice points).
+'   gl_Position = mProj * mView * mWorld * vec4(vertPosition, 0.0, 1.0);',
+'}'
+].join('\n');
+
+var fragmentShaderText =
+[
+'precision mediump float;',
+'',
+'varying vec3 fragColour;',
+'',
+'void main()',
+'{',
+'   gl_FragColor = vec4(fragColour, 1.0);',
+'}'
+].join('\n');
+
+var InitDemo = function () {
+    console.log('This is working.');
+
+    var canvas = document.getElementById('game-surface');
+    var gl = canvas.getContext('webgl');
+
+    if (!gl) {
+        console.log('WebGL not supported, falling back on experimental-webgl');
+        gl = canvas.getContext('experimental-webgl');
+    }
+
+    if (!gl) {
+        alert('Your browser does not support WebGl.');
+    }
+
+    gl.clearColor(0.75, 0.85, 0.8, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vertexShader, vertexShaderText);
+    gl.shaderSource(fragmentShader, fragmentShaderText);
+
+    gl.compileShader(vertexShader);
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+        console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
+        return;
+    }
+
+    gl.compileShader(fragmentShader);
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+        console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
+        return;
+    }
+
+    var program = gl.createProgram();
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    
+    gl.linkProgram(program);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.error('ERROR linking program!', gl.getProgramInfoLog(program));
+        return;
+    }
+
+    gl.validateProgram(program);
+    if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+        console.error('ERROR validating program!', gl.getProgramInfoLog(program));
+        return;
+    }
+
+    var triangleVertices =
+    [ // X, Y, Z         R, G, B
+        0.0, 0.5, 0.0,   1.0, 1.0, 0.0,
+        -0.5, -0.5, 0.0, 0.7, 0.0, 1.0,
+        0.5, -0.5, 0.0,  0.1, 1.0, 0.6
+    ];
+
+    var triangleVertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
+
+    var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
+    var colourAttribLocation = gl.getAttribLocation(program, 'vertColour');
+
+    gl.vertexAttribPointer(
+        positionAttribLocation,
+        // Now that we have z coords, attribute length
+        // is 3.
+        3,
+        gl.FLOAT,
+        gl.FALSE,
+        // There are now 6 values per vertex.
+        6 * Float32Array.BYTES_PER_ELEMENT,
+        0
+    );
+
+    gl.vertexAttribPointer(
+        colourAttribLocation,
+        3,
+        gl.FLOAT,
+        gl.FALSE,
+        // There are now 6 values per vertex.
+        6 * Float32Array.BYTES_PER_ELEMENT,
+        // Greater coordinate length means greater
+        // offset for colour.
+        3 * Float32Array.BYTES_PER_ELEMENT
+    );
+
+    gl.enableVertexAttribArray(positionAttribLocation);
+    gl.enableVertexAttribArray(colourAttribLocation);
+
+    // Setting uniforms
+    //Getting locations
+    var matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
+    var matViewUniformLocation = gl.getUniformLocation(program, 'mView');
+    var matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
+    // Setting initial values to 4x4 arrays of 0s
+    var worldMatrix = new Float32Array(16);
+    var viewMatrix = new Float32Array(16);
+    var projMatrix = new Float32Array(16);
+    // Setting values to identity matrices
+    mat4.identity(worldMatrix);
+    mat4.identity(viewMatrix);
+    mat4.identity(projMatrix);
+
+    gl.useProgram(program);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+}
+
+// NOTE - REACHED 14:52 INTO VIDEO 2
